0349d06a1e5c2f86dbd8243c07e1ac8328fc11e0
PriyankaArora13 <priyankamiddha.13@gmail.com>
2024-04-27 19:57:10 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,21 @@
+/*
+<div id="parent">
+     <div id="child">
+         <h1>Hii i am h1 tag</h1>    //if we want to pass siblings pass it in the from of array
+         <h2>This is an h2 tag</h2>
+      </div>
+</div>
+ */
+
+// ReactElement(Object)=>HTML(Browser understands)
+
+
+const parent=React.createElement("div",{id:"parent"},
+React.createElement(
+"div",{id:"child"},[
+    React.createElement("h1",{},"Hii I am h1 tag"),
+    React.createElement("h2",{},"this is an h2 tag")
+]
+));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
